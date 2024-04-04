84624b67a90512a0bccc1f2fce69e913725b1b55
FAAAAAAR <fargamers22@gmail.com>
2024-04-04 10:26:11 +0700

    v3.0

---
+++ copytext.js
@@ -0,0 +1,48 @@
+document.getElementById("copyButtonML").addEventListener("click", function() {
+    var numberToCopy = "738555334";
+
+    navigator.clipboard.writeText(numberToCopy).then(function() {
+        alert("text berhasil disalin");
+    }, function(err) {
+        console.error("gagal menyalim text: ", err);
+    });
+});
+
+document.getElementById("copyButtonBA").addEventListener("click", function() {
+    var numberToCopy = "AYVBWNGY";
+
+    navigator.clipboard.writeText(numberToCopy).then(function() {
+        alert("text berhasil disalin");
+    }, function(err) {
+        console.error("gagal menyalim text: ", err);
+    });
+});
+
+document.getElementById("copyButtonGT").addEventListener("click", function() {
+    var numberToCopy = "#YW74D3";
+
+    navigator.clipboard.writeText(numberToCopy).then(function() {
+        alert("text berhasil disalin");
+    }, function(err) {
+        console.error("gagal menyalim text: ", err);
+    });
+});
+
+document.getElementById("copyButtonFF").addEventListener("click", function() {
+    var numberToCopy = "487339555";
+
+    navigator.clipboard.writeText(numberToCopy).then(function() {
+        alert("text berhasil disalin");
+    }, function(err) {
+        console.error("gagal menyalim text: ", err);
+    });
+});
+document.getElementById("copyButtonCOC").addEventListener("click", function() {
+    var numberToCopy = "QOVJO2PGC";
+
+    navigator.clipboard.writeText(numberToCopy).then(function() {
+        alert("text berhasil disalin");
+    }, function(err) {
+        console.error("gagal menyalim text: ", err);
+    });
+});
